d39bfe0b14cf3d827d49d8a47d5ca3a69184dca1
Brian Wolf <bwolf1@gmail.com>
2021-02-09 17:26:22 -0600

    updated formatting

--- testserver.py
+++ testserver.py
@@ -1,8 +1,11 @@
 import os
 from http.server import HTTPServer, CGIHTTPRequestHandler
+
 # Ensure that the server is created in a directory that we know is safe to expose
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
 # Create server object listening the port 80
 server_object = HTTPServer(server_address=('', 80), RequestHandlerClass=CGIHTTPRequestHandler)
+
 # Start the web server
 server_object.serve_forever()
