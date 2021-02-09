711d4447b4ac9d378fe7a344723b70de557535f4
Brian Wolf <bwolf1@gmail.com>
2021-02-09 23:24:49 +0000

    removed hard-coded working directory path

--- testserver.py
+++ testserver.py
@@ -1,7 +1,7 @@
 import os
 from http.server import HTTPServer, CGIHTTPRequestHandler
 # Ensure that the server is created in a directory that we know is safe to expose
-os.chdir('/home/brian/test-server')
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 # Create server object listening the port 80
 server_object = HTTPServer(server_address=('', 80), RequestHandlerClass=CGIHTTPRequestHandler)
 # Start the web server
